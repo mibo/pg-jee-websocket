dffbff6d1a5e3259fbb0134b4ed4a53c4dde1c71
Michael <mibo@mirb.de>
2017-02-27 08:38:19 +0100

    Started...

---
+++ pg-ws-server/src/main/java/de/mirb/pg/jee/ws/server/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package de.mirb.pg.jee.ws.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint.
+ * Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ */
+@ApplicationPath("server")
+public class JAXRSConfiguration extends Application {
+
+}
